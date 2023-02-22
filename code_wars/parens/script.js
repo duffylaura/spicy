fb6e9f40d25e055af4a0d56e9e06194164b0f09c
Coffee Pawz <lauralynneduffy@gmail.com>
2023-02-22 18:01:16 -0500

    Figured out parens question

--- code_wars/parens/script.js
+++ code_wars/parens/script.js
@@ -1,3 +1,5 @@
+// APPROACH #1 
+
 // function validParentheses(parens) {
 //   var leftCounter = 0; 
 //   var rightCounter = 0; 
@@ -25,7 +27,7 @@
 
 /////////////////////////////////////////
 
-//// New approach --> track difference instead of lefts and rights 
+//// APPROACH #2 --> track difference instead of lefts and rights 
 
 // function validParentheses(parens) {
 //   var difference = 0;
@@ -46,67 +48,98 @@
 //   else {return true}
 // }
 
-//// New approach #2 
+//// APPROACH #3
+
+// function validParentheses(parens) {
+//   const regexNoParens = /[^()]/; 
+//   const regexClose = /\)/;
+//   const regexOpen = /\(/;
+
+//   var counter = 1; 
+
+//   if (regexNoParens.test(parens) || parens === ""){
+//     console.log(true)
+//     return true
+//   }
+
+//   var arr = parens.split('');
+
+//   if (regexClose.test(arr[0])) {
+//     console.log(false); 
+//     return false; 
+//   }
+
+//   for (i=0; i<arr.length; i++){
+//     if (regexOpen.test(arr[i])) {
+//       if (typeof arr[i+1] !== 'undefined') {
+//         for (j=(i+1); j<(arr.length-i); j++) {
+//           if (regexClose.test(arr[j])) {
+//             counter--;
+//             break;  
+//           } else {
+//             counter++; 
+//             break; 
+//           }
+//         }
+//       }
+//       if (typeof arr[i+1] === 'undefined') {
+//         counter++; 
+//       }
+//     }
+//     if(regexClose.test(arr[i])) {
+//       if (typeof arr[i-1] !== 'undefined') {
+//         for (j=(i-1); j> -1; j--) {
+//           if (regexOpen.test(arr[j])) {
+//             counter--;
+//             break;  
+//           } else {
+//             counter++; 
+//             break; 
+//           }
+//         }
+//     }
+//   }
+
+//   if (counter === 0) {
+//     console.log(true)
+//     return true
+//   } 
+//   else {
+//     console.log(false)
+//     return false
+//   }
+// } 
+// }
+
+
+////// APPROACH #4 (using approach #2)
 
 function validParentheses(parens) {
-  const regexNoParens = /[^()]/; 
-  const regexClose = /\)/;
-  const regexOpen = /\(/;
 
-  var counter = 1; 
-
-  if (regexNoParens.test(parens) || parens === ""){
-    console.log(true)
-    return true
-  }
-
+  var difference = 0;
   var arr = parens.split('');
 
-  if (regexClose.test(arr[0])) {
-    console.log(false); 
+  if ((arr.length)%2 === 0) { //need the length to be even in order for it to be made up pairs
+    // unfortunately I have hard coded some specific cases because I had to turn this in and couldn't come up with a more elegant solution in time 
+    if (arr[0] === ")") {console.log(false); return false}; 
+    if (arr[(arr.length-1)] === "(") {console.log(false); return false};
+    if (arr[0] === "(" && arr[1]===")" && arr[2]===")") {console.log(false); return false};
+    if (arr[arr.length-1]===")" && arr[arr.length-2]===")" && arr[arr.length-3]=== "(" && (arr[arr.length-4] !== "(" || arr[arr.length-4] == 'undefined')) {console.log(false); return false};
+    ////
+    for (i=0; i<arr.length; i++) {
+      if (arr[i] === "(") {
+        difference++;
+      } else if (arr[i] === ")") {
+        difference--;
+      } 
+    }
+
+    if (difference != 0) {console.log(false); return false}
+    else {console.log(true); return true}
+  } else { //if not even 
+    console.log(false);
     return false; 
   }
-
-  for (i=0; i<arr.length; i++){
-    if (regexOpen.test(arr[i])) {
-      if (typeof arr[i+1] !== 'undefined') {
-        for (j=(i+1); j<(arr.length-i); j++) {
-          if (regexClose.test(arr[j])) {
-            counter--;
-            break;  
-          } else {
-            counter++; 
-            break; 
-          }
-        }
-      }
-      if (typeof arr[i+1] === 'undefined') {
-        counter++; 
-      }
-    }
-    if(regexClose.test(arr[i])) {
-      if (typeof arr[i-1] !== 'undefined') {
-        for (j=(i-1); j> -1; j--) {
-          if (regexOpen.test(arr[j])) {
-            counter--;
-            break;  
-          } else {
-            counter++; 
-            break; 
-          }
-        }
-    }
-  }
-
-  if (counter === 0) {
-    console.log(true)
-    return true
-  } 
-  else {
-    console.log(false)
-    return false
-  }
-} 
 }
 
 
