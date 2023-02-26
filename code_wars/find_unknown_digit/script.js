5af6e27fdefec1d781c1d39bceb0014ea6bbd52a
Coffee Pawz <lauralynneduffy@gmail.com>
2023-02-26 14:32:23 -0500

    Working except for example on line 137. Start there

--- code_wars/find_unknown_digit/script.js
+++ code_wars/find_unknown_digit/script.js
@@ -19,12 +19,13 @@ function solveExpression(exp) {
   if (addRegex.test(exp)) {
     var arr = exp.split(/[+=]/);  // arr[0] is first number; arr[1] is second number; arr[2] is answer 
     var quesPositions = []; 
+    
     for (i=0; i<arr.length; i++) {
       if (quesMarkRegex.test(arr[i])) { 
         quesPositions.push(i); //This is so that I am storing the positions of term(s) w/ question marks 
       }
     }
-    console.log(quesPositions)
+    
     if (quesPositions.length === 0) { //no question marks present 
       if (parseInt(arr[0])+parseInt(arr[1]) === parseInt(arr[2])) {
         console.log('There was no rune in this expression and the expression is true.')
@@ -32,44 +33,86 @@ function solveExpression(exp) {
         console.log(-1) //per the requirements, output -1 if professor is wrong 
       }
     }
+    
     if (quesPositions.length === 1) { //one question mark present 
       var outputTerm = []; 
+      var questionMarkPlaceValue = []; 
+      
       if (quesPositions[0] === 0) { // 1st term has the mark
+        let term = arr[quesPositions[0]].split(''); 
+        // console.log(term); 
+        for (i=0; i<term.length; i++) {
+          if (!(quesMarkRegex.test(term[i]))){
+          let x = term[i] * placeValue[term.length][i]; 
+          outputTerm.push(x); 
+          } else {
+            questionMarkPlaceValue.push(i); 
+          }
+        }
+        // console.log(outputTerm); 
+        // console.log(questionMarkPlaceValue); 
+
+        var sum = parseInt(arr[1]); 
+        for (i=0; i<outputTerm.length; i++) { //add output terms together and arr[1] then subtact them from arr[2]
+          sum += parseInt(outputTerm[i]); 
+        }
+        console.log(sum); 
+        var unknown = (parseInt(arr[2]) - sum).toString().split('');
+        // console.log('unknown' + unknown) 
+        // console.log(questionMarkPlaceValue);
+        var output = []; 
+        for (i=0; i<questionMarkPlaceValue.length; i++) {
+          let x = parseInt(questionMarkPlaceValue[i]); 
+          output.push(unknown[x]);
+        }; 
+        if (output.length === 1) {
+          console.log(parseInt(output[0]))
+        } //else what to do if there is more than one question mark 
+      }
+      if (quesPositions[0]==1) { //second term has mark
         let term = arr[quesPositions[0]].split(''); 
         for (i=0; i<term.length; i++) {
           if (!(quesMarkRegex.test(term[i]))){
           let x = term[i] * placeValue[term.length][i]; 
           outputTerm.push(x); 
-          } 
+          } else {
+            questionMarkPlaceValue.push(i); 
+          }
         }
-        var sum = parseInt(arr[1]); 
+        var sum = parseInt(arr[0]); 
         for (i=0; i<outputTerm.length; i++) { //add output terms together and arr[1] then subtact them from arr[2]
           sum += parseInt(outputTerm[i]); 
         }
-        var unknown = parseInt(arr[2]) - sum; 
-        console.log(unknown); 
+        var unknown = (parseInt(arr[2]) - sum).toString().split('');
+        var output = []; 
+        for (i=0; i<questionMarkPlaceValue.length; i++) {
+          let x = parseInt(questionMarkPlaceValue[i]); 
+          output.push(unknown[x]);
+        }; 
+        if (output.length === 1) {
+          console.log(parseInt(output[0]))
+        } //else what to do if there is more than one question mark 
       }
-      if (quesPositions[0]==1) { //second term has mark
-
       }
-      if (quesPositions[0]==2) { //third term has mark
 
-      }   
-    }
-    if (quesPositions.length == 2) { //two question marks present 
-      if(quesPositions[0]==0 && quesPositions[1] ==1) {  // 1 and 2
+    //   if (quesPositions[0]==2) { //third term has mark
 
-      }
-      if(quesPositions[0]==0 && quesPositions[1] ==2) {  // 1 and 3
+    //   }   
+    // }
+    // if (quesPositions.length == 2) { //two question marks present 
+    //   if(quesPositions[0]==0 && quesPositions[1] ==1) {  // 1 and 2
 
-      }
-      if(quesPositions[0]==1 && quesPositions[1] ==2) {  //2 and 3 
+    //   }
+    //   if(quesPositions[0]==0 && quesPositions[1] ==2) {  // 1 and 3
 
-      }
-    }
-    if (quesPositions.length ==3) { //all three terms contain question mark s
+    //   }
+    //   if(quesPositions[0]==1 && quesPositions[1] ==2) {  //2 and 3 
 
-    }
+    //   }
+    // }
+    // if (quesPositions.length ==3) { //all three terms contain question mark s
+
+    // }
   }
 
   //subtraction 
@@ -80,14 +123,18 @@ function solveExpression(exp) {
   if (multRegex.test(exp)) {
 
   }
+}
 
   
-}
+
 
 //Work 
 solveExpression('1+1=2'); 
-solveExpression('?+1=2');
-solveExpression('3?+123=153');
+solveExpression('?+1=2'); //1
+solveExpression('3?+123=153'); //0
+// solveExpression ('??+48=98') // the output of this one is 50; but are the directions saying that ? has to equal itself (e.g. would this be a -1 because 5 does not equal zero; but if the answer was 22 then the output would be 2?)
+solveExpression('40309+3?7=41006') //9 
+solveExpression('1?8+30=198') //6
 
 //In development 
 
