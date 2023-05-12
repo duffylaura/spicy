2853c1973e3d077c124c9eff374ccbcd81f6e188
Coffee Pawz <lauralynneduffy@gmail.com>
2023-05-12 15:01:16 -0400

    pascalCase to string - first thought code - timed output

---
+++ code_wars/exercises/pascalCase_str_to_snake_case/script.js
@@ -0,0 +1,75 @@
+function toUnderscore(pascalCase) {
+    
+    if (typeof pascalCase === 'number') {
+      return pascalCase.toString();
+    }
+    
+    let snakeCase = '';
+       
+    for (let i=0; i<pascalCase.length; i++) {
+        const char = pascalCase[i];
+
+        if (i !== 0) {
+            if (char == '0') {
+                snakeCase += char; 
+                continue; 
+            }
+            if (char == '1') {
+                snakeCase += char; 
+                continue; 
+            }
+            if (char == '2') {
+                snakeCase += char; 
+                continue; 
+            }
+            if (char == '3') {
+                snakeCase += char; 
+                continue; 
+            }
+            if (char == '4') {
+                snakeCase += char; 
+                continue; 
+            }
+            if (char == '5') {
+                snakeCase += char; 
+                continue; 
+            }
+            if (char == '6') {
+                snakeCase += char; 
+                continue; 
+            }
+            if (char == '7') {
+                snakeCase += char; 
+                continue; 
+            }
+            if (char == '8') {
+                snakeCase += char; 
+                continue; 
+            }
+            if (char == '9') {
+                snakeCase += char; 
+                continue; 
+            }
+            if (char !== char.toUpperCase()) {
+                snakeCase += char; 
+                continue; 
+            } 
+            if (char === char.toUpperCase()) {
+                snakeCase += '_'; 
+                snakeCase += char.toLowerCase(); 
+                continue; 
+            }
+        } else 
+        {
+            snakeCase += char.toLowerCase(); 
+            continue; 
+        }
+    }
+
+    return snakeCase;
+}
+
+// Am7Days should equals am7_days 
+  
+
+// My3CodeIs4TimesBetter should equals my3_code_is4_times_better 
